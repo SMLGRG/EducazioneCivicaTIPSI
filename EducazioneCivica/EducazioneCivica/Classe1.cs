74f046c3cdd32473516116e5aca6ac172a8eeb51
SMLGRG <yoygrey21@gmail.com>
2023-04-01 19:17:36 +0200

    Creazione Prima funzionalità

---
+++ EducazioneCivica/EducazioneCivica/Classe1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducazioneCivica
+{
+    internal class Classe1
+    {
+        string alfbt = "abcdefghijklmnopqrstuvwxyz";
+        public static void Primo(string s, int n)
+        { 
+
+        }
+    }
+}
