74a8ff839d9d1c93068cdb5d8f140af95a3faa67
SMLGRG <yoygrey21@gmail.com>
2023-04-02 08:47:48 +0200

    Creazione metodi funzionalità 2

--- EducazioneCivica/EducazioneCivica/Classe1.cs
+++ EducazioneCivica/EducazioneCivica/Classe1.cs
@@ -29,6 +29,27 @@ namespace Nammasp
             char d = char.IsUpper(ch) ? 'A' : 'a';
             return (char)((((ch + key) - d) % 26) + d);
         }
+        public static string Secondo(string input, int key)
+        {
+            string output = "";
+
+            foreach (char ch in input)
+                output += cipher(ch, key);
+
+            return output;
+        }
+        public static char cipher2(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch - key) - d) % 26) + d);
+        }
+
 
         public static void Main(String[] args)
         {
